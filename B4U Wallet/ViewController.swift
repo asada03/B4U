e6bfbd49ded708722513d4dd782198cef682345e
Andres Sada <andres.sada@gmail.com>
2018-01-20 13:48:00 -0600

    Initial Commit

---
+++ B4U Wallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  B4U Wallet
+//
+//  Created by Andres Luis Sada Govela on 20/01/18.
+//  Copyright © 2018 Andres Luis Sada Govela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
